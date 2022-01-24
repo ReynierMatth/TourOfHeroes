622fa4ae3251cf862f280215be2670356d440851
Mashuuu <mashuuu.r@gmail.com>
2022-01-24 11:03:43 +0100

    finish tuto

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id : 11, name: 'Sunny'},
+  {id : 12, name: 'Mashuuu'},
+  {id : 13, name: 'KOOKIE'},
+  {id : 14, name: 'Captain'},
+  {id : 15, name: 'Jocker'},
+  {id : 16, name: 'Twenty one'},
+  {id : 17, name: 'Ama'},
+  {id : 18, name: 'MrMasquime'},
+  {id : 19, name: 'Dadju'},
+  {id : 20, name: 'Iridium'},
+];
